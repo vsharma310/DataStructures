22abf73c0b54d25f18906c00775e37412ec6a479
Vishal Sharma <vishalsharmaoct3@gmail.com>
2022-02-22 16:03:11 -0600

    Applied Higher Order function

--- JSHigherOrderFunction.js
+++ JSHigherOrderFunction.js
@@ -1,33 +1,59 @@
 const radius = [1,2,3,4]; 
 
-const 
+const area = (r) => {
+    return (Math.PI*r*r);
+};
 
-const calculatearea = (r) => {
+const diameter = (r) => {
+    return (2*r); 
+};
+
+const circumference = (r) => {
+    return (2*Math.PI*r);
+};
+
+const calculate = (r, logic) => {
     const a = []; 
     for(i=0; i<= r.length -1; i++){
-        a.push(Math.PI * radius[i] * radius[i]);
+        a.push(logic(r[i]));
     }
     return a;
 };
 
-const calculatediameter = (r) => {
-    const a = []; 
-    for(i=0; i<= r.length -1; i++){
-        a.push(2*radius[i]);
-    }
-    return a;
-}
+console.log(calculate(radius,area)); 
 
-const calculatecircumference = (r) => {
-    const a = []; 
-    for(i=0; i<= r.length -1; i++){
-        a.push(2* Math.PI *radius[i]);
-    }
-    return a;
-}
+console.log(calculate(radius,diameter)); 
 
-console.log(calculatearea(radius));
+console.log(calculate(radius,circumference)); 
 
-console.log(calculatediameter(radius));
 
-console.log(calculatecircumference(radius));
+// const calculatearea = (r) => {
+//     const a = []; 
+//     for(i=0; i<= r.length -1; i++){
+//         a.push(Math.PI * radius[i] * radius[i]);
+//     }
+//     return a;
+// };
+
+// const calculatediameter = (r) => {
+//     const a = []; 
+//     for(i=0; i<= r.length -1; i++){
+//         a.push(2*radius[i]);
+//     }
+//     return a;
+// }
+
+// const calculatecircumference = (r) => {
+//     const a = []; 
+//     for(i=0; i<= r.length -1; i++){
+//         a.push(2* Math.PI *radius[i]);
+//     }
+//     return a;
+// }
+
+
+// console.log(calculatearea(radius));
+
+// console.log(calculatediameter(radius));
+
+// console.log(calculatecircumference(radius));
